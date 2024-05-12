6c81c2a950b1f66b14461fbda93c310de4a74a04
TUTULROY <tutulroyjoy@gmail.com>
2024-05-12 09:25:45 +0600

    created some collection

--- index.js
+++ index.js
@@ -29,12 +29,37 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    const alternativeProductCollection = client.db('alternativeProductDB').collection('products');
+    const userCollection = client.db('alternativeProductDB').collection('user');
+
+
+    app.get('/products', async(req, res) =>{
+        const cursor = alternativeProductCollection.find();
+        const result = await cursor.toArray();
+        res.send(result);
+    })
+    app.get('/user', async(req, res) =>{
+        const cursor = userCollection.find();
+        const result = await cursor.toArray();
+        res.send(result);
+    })
+
+
+
+    app.post('/user', async(req, res) =>{
+        const user = req.body;
+        console.log(user);
+        const result = await userCollection.insertOne(user);
+        res.send(result);
+    })
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
